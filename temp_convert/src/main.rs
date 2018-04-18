8c12b4b373da1417b0580e62d5a5f09b9bb5924f
Brooks Patton <brooks@brookzerker.com>
2018-04-18 08:07:59 -0600

    Refactored to use match, not store everything in the heap, and more!

--- temp_convert/src/main.rs
+++ temp_convert/src/main.rs
@@ -1,52 +1,51 @@
 use std::io;
+use std::ops::Deref;
 
 fn main() {
-    let mut input_temperature;
-    let mut input_symbol;
-
     println!("Temperature converter!");
-    input_temperature = get_temperature();
-    input_symbol = get_temperature_symbol();
+    let mut input_temperature = get_temperature();
+    let mut input_symbol = get_temperature_symbol();
 
-    if "f" == input_symbol || "F" == input_symbol {
-        input_temperature = convert_f_to_c(input_temperature);
-        input_symbol = "c".to_string();
-    } else {
-        input_temperature = convert_c_to_f(input_temperature);
-        input_symbol = "f".to_string();
+    match input_symbol {
+        'f' => {
+            input_temperature = convert_f_to_c(input_temperature);
+            input_symbol = 'c';
+            },
+        'c' => {
+            input_temperature = convert_c_to_f(input_temperature);
+            input_symbol = 'f';
+            },
+        _ => unreachable!(),
     }
     
     println!("Your temperature is {}{}", input_temperature, input_symbol);
 }
 
 fn get_temperature() -> f32 {
-    let message = "What is the temperature you want to convert?".to_string();
+    let message = "What is the temperature you want to convert?";
     let temperature = get_user_input(message);
-    let temperature = temperature.parse::<f32>()
-        .expect("please give me a number next time :(");
-
-    temperature
+    temperature.parse::<f32>()
+        .expect("please give me a number next time :(")    
 }
 
-fn get_temperature_symbol() -> std::string::String {
-    let message = "Is this a temperature in c or f?".to_string();
+fn get_temperature_symbol() -> char {
+    let message = "Is this a temperature in c or f?";
     let temperature_symbol = get_user_input(message);
 
-    if "f" == temperature_symbol || "F" == temperature_symbol || "c" == temperature_symbol || "C" == temperature_symbol {
-        temperature_symbol
-    } else {
-        panic!("Please enter C or F");
+    match temperature_symbol.deref() {
+        "f" | "F" => 'f',
+        "c" | "C" => 'c',
+        _ => panic!("Please enter C or F"),
     }
 }
 
-fn get_user_input(message: std::string::String) -> std::string::String {
+fn get_user_input(message: &str) -> String {
     let mut user_input = String::new();
-    
     println!("{}", message);
     io::stdin().read_line(&mut user_input)
         .expect("I failed to read the line, please try again");
 
-    user_input.trim().to_string()
+    String::from(user_input.trim())
 }
 
 fn convert_f_to_c(temperature: f32) -> f32 {
